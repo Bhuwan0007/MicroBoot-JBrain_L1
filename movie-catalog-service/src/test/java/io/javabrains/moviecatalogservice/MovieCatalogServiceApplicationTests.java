724baa9c752f6b01a75929c62b304d1e90d5059d
Bhuwan <bhuwan0007@gmail.com>
2021-10-03 01:54:51 +0530

    All Projects commit

---
+++ movie-catalog-service/src/test/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package io.javabrains.moviecatalogservice;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ rating-data-service/src/main/java/io/javabrains/ratingdataservice/resources/RatingDataResource.java
@@ -0,0 +1,28 @@
+package io.javabrains.ratingdataservice.resources;
+
+import io.javabrains.ratingdataservice.models.Rating;
+import io.javabrains.ratingdataservice.models.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ratingsData")
+public class RatingDataResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+}
